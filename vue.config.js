b45558583095f56f5a2eecc49a61226a1882819d
Sova <sova123409@gmail.com>
2021-01-17 21:29:44 +0300

    ci: :bug: Fix github pages envs

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const process = require('process');
 
-console.log(process.env.PUBLIC_PATH);
-console.log(process.env, Object.keys(process.env));
-
 module.exports = {
   publicPath: process.env.PUBLIC_PATH || '',
 };
