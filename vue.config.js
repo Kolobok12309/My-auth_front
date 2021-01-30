b3f8664b8822dbf68b22a6787ab22e89f2833deb
Sova <sova123409@gmail.com>
2021-01-30 21:35:52 +0300

    build: :art: Fix vue files import

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const process = require('process');
 
 module.exports = {
   publicPath: process.env.PUBLIC_PATH || '',
+
+  configureWebpack: (config) => {
+    const extensionsWithoutVue = config.resolve.extensions.filter((ext) => ext !== '.vue');
+
+    config.resolve.extensions = ['.vue', ...extensionsWithoutVue];
+  },
 };
