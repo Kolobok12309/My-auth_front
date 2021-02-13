6d07900baeefc934ebc01530f412b151e5717eb0
Sova <sova123409@gmail.com>
2021-02-13 16:40:16 +0300

    feat: :lipstick: Localize PrimeVue

--- src/main.js
+++ src/main.js
@@ -25,7 +25,30 @@ const initApp = async () => {
 
   return app
     .use(getRouter(store))
-    .use(PrimeVue, { ripple: true })
+    .use(PrimeVue, {
+      ripple: true,
+      locale: {
+        accept: 'Да',
+        reject: 'Нет',
+        choose: 'Выбрать',
+        upload: 'Загрузить',
+        cancel: 'Отмена',
+        clear: 'Очистить',
+        dayNames: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+        dayNamesShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthNamesShort: ['Янв', 'Фев', 'Мрт', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Снт', 'Окт', 'Нбр'],
+        today: 'Сегодня',
+        weekHeader: 'Нд',
+        firstDayOfWeek: 1,
+        dateFormat: 'dd.mm.yy',
+        weak: 'Слабый',
+        medium: 'Средний',
+        strong: 'Сильный',
+        passwordPrompt: 'Введите пароль',
+      },
+    })
     .use(ToastService);
 };
 
